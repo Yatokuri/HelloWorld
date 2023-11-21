7dabb2640992ae6f58699b70173985ec3b1d7167
Yatokuri <144425585+Yatokuri@users.noreply.github.com>
2023-11-21 11:11:11 +0100

    Update Main.java

--- StringExcersisesClass/src/dk/javahandson/Main.java
+++ StringExcersisesClass/src/dk/javahandson/Main.java
@@ -16,7 +16,7 @@ public class Main {
     private static void exercise1() // From any user input surround the input with tags like <input>. Print the result
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter:");
+        System.out.println("Enter a new sentence:");
         String input = scanner.nextLine();
 
         System.out.println("<" + input + ">");
